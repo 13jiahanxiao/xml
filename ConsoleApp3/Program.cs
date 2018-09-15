7cdf7af2a3bbfc789cb793ed67b62520035b89d7
13jiahanxiao <2565659862@qq.com>
2018-09-15 10:45:13 +0800

    添加项目文件。

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Xml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument employees1 =
+                new XDocument
+                (
+                    new XElement("Employees",
+                        new XElement("Employee1",
+                            new XElement("Name", "Bob"),
+                            new XElement("PhoneNamber", "12345-405")),
+                        new XElement("Employee2",
+                            new XElement("Name", "Sally"),
+                            new XElement("PhoneNmeber", "10213=405")))
+                );
+            employees1.Save("employees.xml");
+            WriteLine(employees1);
+            ReadKey();
+        }
+    }
+}
