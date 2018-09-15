84fd24a55f887fc127d2165b7feb4ad2b91f8314
13jiahanxiao <2565659862@qq.com>
2018-09-15 10:58:32 +0800

    xml的操作1

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -17,13 +17,25 @@ namespace Xml
                     new XElement("Employees",
                         new XElement("Employee1",
                             new XElement("Name", "Bob"),
-                            new XElement("PhoneNamber", "12345-405")),
+                            new XElement("PhoneNumber", "12345-405")),
                         new XElement("Employee2",
                             new XElement("Name", "Sally"),
-                            new XElement("PhoneNmeber", "10213=405")))
+                            new XElement("PhoneNumber", "10213=405")))
                 );
             employees1.Save("employees.xml");
             WriteLine(employees1);
+            XElement root = employees1.Element("Employees");
+            IEnumerable<XElement> employees = root.Elements();
+            foreach(XElement emp in employees)
+            {
+                XElement empNameNode = emp.Element("Name");
+                WriteLine(empNameNode.Value);
+                IEnumerable<XElement> empPhone = emp.Elements("PhoneNumber");
+                foreach(XElement phone in empPhone)
+                {
+                    WriteLine($"{phone.Value}");
+                }
+            }
             ReadKey();
         }
     }
