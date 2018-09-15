9a26f6555a539e4bec97be13a43cdddacc845779
13jiahanxiao <2565659862@qq.com>
2018-09-15 11:07:46 +0800

    xml文档XElement的添加

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -22,20 +22,25 @@ namespace Xml
                             new XElement("Name", "Sally"),
                             new XElement("PhoneNumber", "10213=405")))
                 );
-            employees1.Save("employees.xml");
-            WriteLine(employees1);
             XElement root = employees1.Element("Employees");
             IEnumerable<XElement> employees = root.Elements();
-            foreach(XElement emp in employees)
+            foreach (XElement emp in employees)
             {
                 XElement empNameNode = emp.Element("Name");
                 WriteLine(empNameNode.Value);
                 IEnumerable<XElement> empPhone = emp.Elements("PhoneNumber");
-                foreach(XElement phone in empPhone)
+                foreach (XElement phone in empPhone)
                 {
                     WriteLine($"{phone.Value}");
                 }
             }
+            WriteLine(employees1);
+            WriteLine();
+            XElement rt = employees1.Element("Employees");
+            rt.Add(new XElement("second",new XElement("second1")));
+            rt.Add(new XElement("third"), new XElement("forth"));
+            WriteLine(employees1);
+            employees1.Save("employees.xml");
             ReadKey();
         }
     }
